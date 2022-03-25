611d73dcac6cd2ada75e636bd40f1f04370d4df0
Ruslan Babin <vasus714@yandex.ru>
2022-03-26 00:29:09 +0300

    feat: infection simulator

---
+++ infectionSimulator/main.cpp
@@ -0,0 +1,17 @@
+#include "InfectionSimulator.h"
+
+using namespace infection_simulator;
+
+int main() {
+    std::cout << "Input iterations count: ";
+    int iterationsCount = 0;
+    std::cin >> iterationsCount;
+
+    InfectionSimulator infectionSimulator = InfectionSimulator();
+    for (int i = 0; i < iterationsCount; ++i) {
+        std::cout << infectionSimulator;
+        infectionSimulator.nextIteration();
+    }
+
+    return 0;
+}
